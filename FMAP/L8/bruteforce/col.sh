ea09f47b9b63f751e4d653bcaabb9bbdf656b60a
hzhou43 <87381426+hzhou43@users.noreply.github.com>
2021-12-22 15:38:40 -0600

    Delete FMAP directory

--- FMAP/L8/bruteforce/col.sh
+++
@@ -1,8 +0,0 @@
-for T in 0.65 0.70 0.75 0.80 0.85 0.90 0.95 1.00 1.05 1.10 1.15
-do
-	cat /dev/null >${T}/res.txt
-	for j in `seq -w 6 6 90`
-	do
-                grep -e "#Density" ${T}/out.0.${j} |awk '{print $2,$8}' >>${T}/res.txt 
-        done
-done
